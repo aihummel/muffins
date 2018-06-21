f01d5aa53670817d578a410cb3dcd41304abc35c
aihummel <lexa@lexahummel.net>
2018-06-21 19:41:22 -0400

    cleaned up formula case analysis, supports subtraction
    I hope there wasn’t an easier way to do this

--- procgen.py
+++ procgen.py
@@ -1,49 +1,42 @@
 from fractions import Fraction as Fr
-import re
+
 
 # @author: Lexa
 
 # TODO by MON, input easy, email
-class Proc:
-    def __init__(self, m, s, text, numbers):
-        self.m = m
-        self.s = s
-        self.text = text
-        self.numbers = numbers
+
 
 def main():
-#    text = \
-#        """
-#$f(%s,%s)$
-#$f(%s,%s)=%s$
-#1. %s muffins divided $(%s, %s)$.
-#2. %s muffin divided $(%s, %s)$.
-#3. Give %s students %s shares of size %s.
-#4. Give %s students %s share of size %s and %s shares of size %s.
-##"""
-#
-#    numbers1 = [Fr(5), Fr(4), Fr(5), Fr(4), Fr(3, 8), Fr(4), Fr(3, 8)]
-#    numbers1.extend([Fr(5, 8), Fr(1), Fr(1, 2), Fr(1, 2), Fr(2), Fr(2)]) 
-#    numbers1.extend([Fr(5, 8), Fr(2), Fr(1), Fr(1, 2), Fr(2), Fr(3, 8)])
-#    
-#    numbers2 = [Fr(9), Fr(4), Fr(9), Fr(4), Fr(7, 16), Fr(8), Fr(7, 16)] 
-#    numbers2.extend([Fr(9, 16), Fr(1), Fr(1, 2), Fr(1, 2), Fr(2), Fr(4)]) 
-#    numbers2.extend([Fr(9, 16), Fr(2), Fr(1), Fr(1, 2), Fr(4), Fr(7, 16)])
-#
-#    oproc1 = Proc(5, 4, text, numbers1)
-#    oproc2 = Proc(9, 4, text, numbers2)
-    
-    proc1 = parse_proc("proc1.tex")
-    proc2 = parse_proc("proc2.tex")
-    
-#    print(mkproc((oproc1, oproc2)))
+    text = \
+        """
+f(%s,%s)
+f(%s,%s)=%s
+1. %s muffins divided (%s, %s)
+2. %s muffin divided (%s, %s)
+3. Give %s students %s shares of size %s
+4. Give %s students %s share of size %s and %s shares of size %s
+"""
+
+    numbers1 = [Fr(5), Fr(4), Fr(5), Fr(4), Fr(3, 8), Fr(4), Fr(3, 8)]
+    numbers1.extend([Fr(5, 8), Fr(1), Fr(1, 2), Fr(1, 2), Fr(2), Fr(2)])
+    numbers1.extend([Fr(5, 8), Fr(2), Fr(1), Fr(1, 2), Fr(2), Fr(3, 8)])
+
+    numbers2 = [Fr(9), Fr(4), Fr(9), Fr(4), Fr(7, 16), Fr(8), Fr(7, 16)]
+    numbers2.extend([Fr(9, 16), Fr(1), Fr(1, 2), Fr(1, 2), Fr(2), Fr(4)])
+    numbers2.extend([Fr(9, 16), Fr(2), Fr(1), Fr(1, 2), Fr(4), Fr(7, 16)])
+
+    proc1 = Proc(5, 4, text, numbers1)
+    proc2 = Proc(9, 4, text, numbers2)
     print(mkproc((proc1, proc2)))
 
-# Make proc for f(N*k+M, N) given procs for f(N*A+M, N) and f(N*B+M, N).
-# ex_procs is tuple of two example f(N*k+M, N) Proc instances
-# determine N, M, and ks
+
+# TODO make proc for f(N*k+j, N) given procs for f(N*k+A, N) and f(N*k+B, N)
+# AAAAAaaaAAAaaAAAaAHHHhhhHHhhHhhHHHHHHh
+
+# make proc for f(N*k+M, N) given procs for f(N*A+M, N) and f(N*B+M, N)
 def mkproc(ex_procs):
-    
+    # ex_procs is tuple of two example f(N*k+M, N) Proc instances
+    # determine N, M, and k's
     assert ex_procs[0].s == ex_procs[1].s
     N = ex_procs[0].s
     assert ex_procs[0].m % N == ex_procs[1].m % N
@@ -51,18 +44,18 @@ def mkproc(ex_procs):
     k0 = (ex_procs[0].m - M) / N
     k1 = (ex_procs[1].m - M) / N
 
-    # Fit linear equations for each n=mk+b (num. and denom. separate)
+    # fit linear equations for each n=mk+b (num. and denom. separate)
     assert ex_procs[0].text == ex_procs[1].text
     formulae = []
     for i in range(len(ex_procs[0].numbers)):
         n0 = ex_procs[0].numbers[i]
         n1 = ex_procs[1].numbers[i]
-        m_num = (n1.numerator - n0.numerator)/(k1 - k0)
+        m_num = (n1.numerator - n0.numerator) / (k1 - k0)
         b_num = n0.numerator - m_num * k0
         m_denom = (n1.denominator - n0.denominator) / (k1 - k0)
         b_denom = n0.denominator - m_denom * k0
 
-        # Make coefficients integers
+        # make coefficients integers
         if int(m_num) != m_num:
             m_num_frac = Fr(m_num)
             d = m_num_frac.denominator
@@ -102,203 +95,165 @@ def mkproc(ex_procs):
                 b_denom /= factor
                 break
 
-        # make string for formula accounting for cases of m and b
-        # TODO support negative b
-        if m_num == 0:
-            if m_denom == 0:
-                if b_denom == 1:
-                    # b
-                    formulae.append("%d" % b_num)
-                else:
-                    # b0/b1
-                    formulae.append(r"\frac{%d}{%d}" % (b_num, b_denom))
-            elif m_denom == 1:
-                if b_denom == 0:
-                    # b/k
-                    formulae.append(r"\frac{%d}{k}" % b_num)
-                else:
-                    # b0/(k + b1)
-                    formulae.append(r"\frac{%d}{k + %d}" % (b_num, b_denom))
-            else:
-                if b_denom == 0:
-                    # b/mk
-                    formulae.append(r"\frac{%d}{%dk}" % (b_num, m_denom))
-                else:
-                    # b0/(mk + b1)
-                    formulae.append(r"\frac{%d}{%dk + %d}" % (b_num, m_denom, b_denom))
-        elif m_num == 1:
-            if m_denom == 0:
-                if b_num == 0:
-                    if b_denom == 1:
-                        # k
-                        formulae.append("k")
-                    else:
-                        # k/b
-                        formulae.append(r"\frac{k}{%d}" % b_denom)
-                else:
-                    if b_denom == 1:
-                        # k + b
-                        formulae.append("k + %d" % b_num)
-                    else:
-                        # (k + b0)/b1
-                        formulae.append(r"\frac{k + %d}{%d}" % (b_num, b_denom))
-            elif m_denom == 1:
-                if b_num == 0:
-                    if b_denom == 0:
-                        # 1
-                        formulae.append("1")
-                    else:
-                        # k/(k + b)
-                        formulae.append(r"\frac{k}{k + %d}" % b_denom)
-                else:
-                    if b_denom == 0:
-                        # (k + b)/k
-                        formulae.append(r"\frac{k + %d}{k}" % b_num)
-                    else:
-                        # (k + b0)/(k + b1)
-                        formulae.append(r"\frac{k + %d}{k + %d}" % (b_num, b_denom))
-            else:
-                if b_num == 0:
-                    if b_denom == 0:
-                        # 1/m
-                        formulae.append(r"\frac{1}{%d}" % m_denom)
-                    else:
-                        # k/(mk + b)
-                        formulae.append(r"\frac{k}{%dk + %d}" % (m_denom, b_denom))
-                else:
-                    if b_denom == 0:
-                        # (k + b)/mk
-                        formulae.append(r"\frac{k + %d}{%dk}" % (b_num, m_denom))
-                    else:
-                        # (k + b0)/(mk + b1)
-                        formulae.append(r"\frac{k + %d}{%dk + %d}" % (b_num, m_denom, b_denom))
-        else:
-            if m_denom == 0:
-                if b_num == 0:
-                    if b_denom == 1:
-                        # mk
-                        formulae.append("%dk" % m_num)
-                    else:
-                        # mk/b
-                        formulae.append(r"\frac{%dk}{%d}" % (m_num, b_denom))
-                else:
-                    if b_denom == 1:
-                        # mk + b
-                        formulae.append("%dk + %d" % (m_num, b_num))
-                    else:
-                        # (mk + b0)/b1
-                        formulae.append(r"\frac{%dk + %d}{%d}" % (m_num, b_num, b_denom))
-            elif m_denom == 1:
-                if b_num == 0:
-                    if b_denom == 0:
-                        # m
-                        formulae.append("%d" % m_num)
-                    else:
-                        # mk/(k + b)
-                        formulae.append(r"\frac{%dk}{k + %d}" % (m_num, b_denom))
-                else:
-                    if b_denom == 0:
-                        # (mk + b)/k
-                        formulae.append(r"\frac{%dk + %d}{k}" % (m_num, b_num))
-            else:
-                if b_num == 0:
-                    if b_denom == 0:
-                        # m0/m1
-                        formulae.append(r"\frac{%d}{%d}" % (m_num, m_denom))
-                    else:
-                        # m0k/(m1k + b)
-                        formulae.append(r"\frac{%dk}{%dk + %d}" % (m_num, m_denom, b_denom))
-                else:
-                    if b_denom == 0:
-                        # (m0k + b)/m1k
-                        formulae.append(r"\frac{%dk + %d}{%dk}" % (m_num, b_num, m_denom))
-                    else:
-                        # (m0k + b0)/(m1k + b1)
-                        formulae.append(r"\frac{%dk + %d}{%dk + %d}" % (m_num, b_num, m_denom, b_denom))
+        # check if numerator is multiple of denominator
+        if m_num == m_denom == 0 or b_num == b_denom == 0 or \
+                (m_denom != 0 and b_denom != 0 and m_num / m_denom == b_num / b_denom):
+            frac = Fr(b_num/b_denom) if m_denom == 0 else Fr(m_num/m_denom)
+            m_num = 0
+            m_denom = 0
+            b_num = frac.numerator
+            b_denom = frac.denominator
 
+        # make sure denominator isn't too negative
+        if m_denom < 0 or m_denom == 0 and b_denom < 0:
+            m_num *= -1
+            m_denom *= -1
+            b_num *= -1
+            b_denom *= -1
+
+        # insert string for formula accounting for cases of m and b
+        formulae.append(formula_string(m_num, m_denom, b_num, b_denom))
+
+    # insert formulae
+    # print(formulae)
     return ex_procs[0].text % tuple(formulae)
 
-# Returns the position of the last character of s in text.
-def my_index(text, s):
-    return text.index(s) + len(s) - 1
 
-# input: file w/ LaTeX source code of proc w/ numerical values (e.g. 5, 4/5) 
-# instead of formulae
+# make string for formula accounting for cases of m and b
+def formula_string(m_num, m_denom, b_num, b_denom):
+    if m_num == m_denom == 0 and b_denom == 1:  # b
+        return "$%d$" % b_num
+    elif m_num == m_denom == 0 and b_denom > 0:  # b0/b1
+        return "$\\frac{%d}{%d}$" % (b_num, b_denom)
+    elif m_num != 0 and m_denom == 1 and b_num == b_denom == 0:  # m
+        return "$%d$" % m_num
+    elif m_num != 0 and m_denom != 0 and b_num == b_denom == 0:  # m0/m1
+        return "$\\frac{%d}{%d}$" % (m_num, m_denom)
+    elif m_num == b_denom == 1 and m_denom == b_num == 0:  # k
+        return "$k$"
+    elif m_num == -b_denom == -1 and m_denom == b_num == 0:  # -k
+        return "$-k$"
+    elif m_num == b_denom == 1 and m_denom == 0 and b_num > 0:  # k + b
+        return "$k + %d$" % b_num
+    elif m_num == b_denom == 1 and m_denom == 0 and b_num < 0:  # k - b
+        return "$k - %d$" % -b_num
+    elif m_num == -b_denom == -1 and m_denom == 0 and b_num > 0:  # -k + b
+        return "$-k + %d$" % b_num
+    elif m_num == -b_denom == -1 and m_denom == 0 and b_num < 0:  # -k - b
+        return "$-k - %d$" % -b_num
+    elif m_num not in (0, 1, -1) and m_denom == b_num == 0 and b_denom == 1:  # mk
+        return "$%dk$" % m_num
+    elif m_num not in (0, 1, -1) and m_denom == 0 and b_num > 0 and b_denom == 1:  # mk + b
+        return "$%dk + %d$" % (m_num, b_num)
+    elif m_num not in (0, 1, -1) and m_denom == 0 and b_num < 0 and b_denom == 1:  # mk - b
+        return "$%dk - %d$" % (m_num, -b_num)
+    elif m_num == b_denom == 0 and m_denom == 1 and b_num != 0:  # b/k
+        return "$\\frac{%d}{k}$" % b_num
+    elif m_num == b_denom == 0 and m_denom > 0 and b_num != 0:  # b/mk
+        return "$\\frac{%d}{%dk}$" % (b_num, m_denom)
+    elif m_num == 1 and m_denom == 0 and b_num > 0 and b_denom > 0:  # (k + b0)/b1
+        return "$\\frac{k + %d}{%d}$" % (b_num, b_denom)
+    elif m_num == 1 and m_denom == 0 and b_num < 0 < b_denom:  # (k - b0)/b1
+        return "$\\frac{k - %d}{%d}$" % (-b_num, b_denom)
+    elif m_num == -1 and m_denom == 0 and b_num > 0 and b_denom > 0:  # (-k + b0)/b1
+        return "$\\frac{-k + %d}{%d}$" % (b_num, b_denom)
+    elif m_num == -1 and m_denom == 0 and b_num < 0 < b_denom:  # (-k - b0)/b1
+        return "$\\frac{-k - %d}{%d}$" % (-b_num, b_denom)
+    elif m_num not in (0, -1, 1) and m_denom == 0 and b_num > 0 and b_denom > 0:  # (mk + b0)/b1
+        return "$\\frac{%dk + %d}{%d}$" % (m_num, b_num, b_denom)
+    elif m_num not in (0, -1, 1) and m_denom == 0 and b_num < 0 < b_denom:  # (mk - b0)/b1
+        return "$\\frac{%dk - %d}{%d}$" % (m_num, -b_num, b_denom)
+    elif m_num != 0 and m_denom == 1 and b_num > 0 and b_denom == 0:  # m + b/k
+        return "$%d + \\frac{%d}{k}$" % (m_num, b_num)
+    elif m_num != 0 and m_denom == 1 and b_num < 0 and b_denom == 0:  # m - b/k
+        return "$%d - \\frac{%d}{k}$" % (m_num, -b_num)
+    elif m_num != 0 and m_denom > 1 and b_num > 0 and b_denom == 0:  # (m0k + b)/m1k
+        return "$\\frac{%dk + %d}{%dk}$" % (m_num, b_num, m_denom)
+    elif m_num != 0 and m_denom > 1 and b_num < 0 and b_denom == 0:  # (m0k - b)/m1k
+        return "$\\frac{%dk - %d}{%dk}$" % (m_num, -b_num, m_denom)
+    elif m_num == 0 and m_denom == 1 and b_num != 0 and b_denom > 0:  # b0/(k + b1)
+        return "$\\frac{%d}{k + %d}$" % (b_num, b_denom)
+    elif m_num == 0 and m_denom == 1 and b_num != 0 and b_denom < 0:  # b0/(k - b1)
+        return "$\\frac{%d}{k - %d}$" % (b_num, -b_denom)
+    elif m_num == 0 and m_denom > 1 and b_num != 0 and b_denom > 0:  # b0/(mk + b1)
+        return "$\\frac{%d}{%dk + %d}$" % (b_num, m_denom, b_denom)
+    elif m_num == 0 and m_denom > 1 and b_num != 0 and b_denom < 0:  # b0/(mk - b1)
+        return "$\\frac{%d}{%dk - %d}$" % (b_num, m_denom, -b_denom)
+    elif m_num == m_denom == 1 and b_num == 0 and b_denom > 0:  # k/(k + b)
+        return "$\\frac{k}{k + %d}$" % b_denom
+    elif m_num == m_denom == 1 and b_num == 0 and b_denom < 0:  # k/(k - b)
+        return "$\\frac{k}{k - %d}$" % -b_denom
+    elif m_num == -m_denom == -1 and b_num == 0 and b_denom > 0:  # -k/(k + b)
+        return "$\\frac{-k}{k + %d}$" % b_denom
+    elif m_num == -m_denom == -1 and b_num == 0 and b_denom < 0:  # -k/(k - b)
+        return "$\\frac{-k}{k - %d}$" % -b_denom
+    elif m_num == 1 and m_denom > 1 and b_num == 0 and b_denom > 0:  # k/(mk + b)
+        return "$\\frac{k}{%dk + %d}$" % (m_denom, b_denom)
+    elif m_num == 1 and m_denom > 1 and b_num == 0 and b_denom < 0:  # k/(mk - b)
+        return "$\\frac{k}{%dk - %d}$" % (m_denom, -b_denom)
+    elif m_num == -1 and m_denom > 1 and b_num == 0 and b_denom > 0:  # -k/(mk + b)
+        return "$\\frac{-k}{%dk + %d}$" % (m_denom, b_denom)
+    elif m_num == -1 and m_denom > 1 and b_num == 0 and b_denom < 0:  # -k/(mk - b)
+        return "$\\frac{-k}{%dk - %d}$" % (m_denom, -b_denom)
+    elif m_num not in (0, 1, -1) and m_denom > 1 and b_num == 0 and b_denom > 0:  # m0k/(m1k + b)
+        return "$\\frac{%dk}{%dk + %d}$" % (m_num, m_denom, b_denom)
+    elif m_num not in (0, 1, -1) and m_denom > 1 and b_num == 0 and b_denom < 0:  # m0k/(m1k - b)
+        return "$\\frac{%dk}{%dk - %d}$" % (m_num, m_denom, -b_denom)
+    elif m_num not in (0, 1, -1) and m_denom > 1 and b_num > 0 and b_denom > 0:  # (m0k + b0)/(m1k + b1)
+        return "$\\frac{%dk + %d}{%dk + %d}$" % (m_num, b_num, m_denom, b_denom)
+    elif m_num not in (0, 1, -1) and m_denom > 1 and b_num < 0 < b_denom:  # (m0k - b0)/(m1k + b1)
+        return "$\\frac{%dk - %d}{%dk + %d}$" % (m_num, -b_num, m_denom, b_denom)
+    elif m_num not in (0, 1, -1) and m_denom > 1 and b_denom < 0 < b_num:  # (m0k + b0)/(m1k - b1)
+        return "$\\frac{%dk + %d}{%dk - %d}$" % (m_num, b_num, m_denom, -b_denom)
+    elif m_num not in (0, 1, -1) and m_denom > 1 and b_num < 0 and b_denom < 0:  # (m0k - b0)/(m1k - b1)
+        return "$\\frac{%dk - %d}{%dk - %d}$" % (m_num, -b_num, m_denom, -b_denom)
+
+
+# TODO
+# input: file w/ LaTeX source code of proc w/ numerical values (e.g. 5, 4/5) instead of formulae
 # output: Proc object
 def parse_proc(proc_file):
+    # prox_txt is str: name of file
     infile = open(proc_file)
 
     text = infile.read()
-    orig = text
-    
-    # Removes enumerating numbers.
-    to_remove = [str(s) for s in re.findall(r'\d+\.', text)]
-    
-    for s in to_remove:
-        text = text.replace(s, "") 
-      
-    nums = []
-    str_nums = re.findall(r'\d+', text)
-    
-    # Finds numbers in original text and determines what the fractions are.
-    for s in str_nums:
-        if text[my_index(text, s) + 2] == '{':
-            num = text[my_index(text, s) + 3: my_index(text, s) + len(s) + 4]
-            num = num.replace("}", "")
-            to_add = Fr(int(s), int(num))
-            nums.append(to_add) 
-            text = text.replace(s, "", 1) 
-        elif text[my_index(text, s) + 1] == '}':
-            text = text.replace(s, "", 1) 
+
+    # read m and s
+    function_start = text.find("f")
+    open_paren = text[function_start:].find("(") + function_start
+    comma = text[open_paren:].find(",") + open_paren
+    close_paren = text[comma:].find(")") + comma
+    m = text[open_paren + 1:comma].strip()
+    assert m.isdigit()
+    m = int(m)
+    s = text[comma + 1:close_paren].strip()
+    assert s.isdigit()
+    s = int(s)
+
+    # isolate number values and format text
+    # assumes fractions are formatted \frac{a}{b} with no space
+    i = 0
+    numbers = []
+    while i < len(text):
+        if text[i].isdigit():
+            j = i + 1
+            while text[j].isdigit():
+                j += 1
+            if i > 0 and text[i - 7:i - 1] == r"\frac{" and text[j:j + 2] == "}{" and text[j + 2].isdigit():
+                k = text[j + 3:].find("}") + j + 3
+
         else:
-            text = text.replace(s, "", 1) 
-            nums.append(Fr(int(s)))
-     
-    # Replaces numbers in original with placeholders.
-    for s in str_nums:
-        orig = orig.replace(s, "%s")
-        
-    for s in to_remove:
-        orig = orig.replace(s, "%s.")
-        
-    i = 1    
-    while "%s." in orig:
-        orig = orig.replace("%s.", str(i) + ".", 1)
-        i = i + 1
-        
-    orig = orig.replace("\\frac{%s}{%s}", "%s")
-        
-    # Creates Proc object.
-    proc = Proc(nums[0].numerator, nums[1].numerator, orig, nums) 
-    
-    return proc
-    
-#    # read m and s
-#    function_start = text.find("f")
-#    open_paren = text[function_start:].find("(") + function_start
-#    comma = text[open_paren:].find(",") + open_paren
-#    close_paren = text[comma:].find(")") + comma
-#    m = text[open_paren+1:comma].strip()
-#    assert m.isdigit()
-#    m = int(m)
-#    s = text[comma+1:close_paren].strip()
-#    assert s.isdigit()
-#    s = int(s)
-#
-#    # isolate number values and format text
-#    # assumes fractions are formatted \frac{a}{b} with no space
-#    i = 0
-#    numbers = []
-#    while i < len(text):
-#        if text[i].isdigit():
-#            j = i + 1
-#            while text[j].isdigit():
-#                j += 1
-#            if i > 0 and text[i-7:i-1] == r"\frac{" and text[j:j+2] == "}{" and text[j+2].isdigit():
-#                k = text[j+3:].find("}") + j+3
-#
-#        else:
-#            i += 1
+            i += 1
+
+
+class Proc:
+    def __init__(self, m, s, text, numbers):
+        self.m = m
+        self.s = s
+        # self.y = y
+        self.text = text
+        self.numbers = numbers
+
 
 if __name__ == "__main__":
     main()
